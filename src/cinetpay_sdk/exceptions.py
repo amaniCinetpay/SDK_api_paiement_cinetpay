04ebf6adc4afc7d507d6853d63c6880c8d8f0cac
amaniCinetpay <amani.kouakou@cinetpay.com>
2021-08-18 12:15:32 +0000

    exception updated with Tacka

--- src/cinetpay_sdk/exceptions.py
+++ src/cinetpay_sdk/exceptions.py
@@ -2,25 +2,12 @@ class Exceptions :
     def __init__(self) :
         pass
     def exceptions(self,data) :
-        if "amount" not in data :
-            msg = "amount is required"
-            return msg
-        elif "currency" not in  data:
-            msg = "currency is required"
-            return msg
-        elif "return_url" not in  data:
-            msg = "return_url is required"
-            return msg
-        elif "notify_url" not in  data:
-            msg = "notify_url is required"
-            return msg
-        elif "customer_name" not in  data:
-            msg = "customer_name is required"
-            return msg
-        elif "customer_surname" not in  data:
-            msg = "customer_surname is required"
-            return msg
-        elif "transaction_id" not in  data:
-            msg = "transaction_id is required"
-            return msg
+        KEYS = ["amount", "currency", "return_url", "notify_url", "customer_name", "customer_surname", "transaction_id"]
+        to_add = []
+        for key in KEYS :
+            if key not in list(data.keys()) :
+                to_add.append("{} is required".format(key))
+
+        if  to_add != [] : 
+            return to_add
         
